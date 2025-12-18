8989d7d587592889c70040b5285ae16c6d94759f
Yaroslav Petrov <iring2209@gmail.com>
2025-12-18 22:10:23 +0100

    Fourth Commit

---
+++ ДЗ 3.2. Числа та рядки/index.js
@@ -0,0 +1,7 @@
+let firstLine = prompt('Введіть перший рядок')
+
+let secondLine = prompt('Введіть другий рядок')
+
+let thirdLine = prompt('Введіть третій рядок')
+
+console.log(`Перший рядок:${firstLine}, другий рядок:${secondLine}, третій рядок:${thirdLine}`)
