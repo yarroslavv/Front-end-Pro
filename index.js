f21941dd6632a985a07378c649618ff70b558b1a
Yaroslav Petrov <iring2209@gmail.com>
2025-12-18 20:24:28 +0100

    Second Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof 78347528743608130698710349763029887656758765n);
+console.log(typeof Symbol('id'));
