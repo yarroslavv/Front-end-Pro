341931a6ae386ac47331699af89148c65f0557fd
Yaroslav Petrov <iring2209@gmail.com>
2025-12-19 17:27:36 +0100

    Sixth Commit

--- ДЗ 3.3. Розкласти п'ятизначне число/index.js
+++ ДЗ 3.3. Розкласти п'ятизначне число/index.js
@@ -1,6 +1,14 @@
 function numberSeparation (){
     let num = '83502'
+    let result = ''
 
-    console.log(`${num[0]} ${num[1]} ${num[2]} ${num[3]} ${num[4]}`)
+    // console.log(`${num[0]} ${num[1]} ${num[2]} ${num[3]} ${num[4]}`)
+
+    for (let i = 0; i < num.length; i++){
+        result += num[i] + ' '
+    }
+    console.log(result.trim())
 }
 numberSeparation()
+
+
