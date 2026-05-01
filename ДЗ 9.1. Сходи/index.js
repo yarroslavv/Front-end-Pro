822bb4a01714bc4aed09243fce45ecd3baa3723a
Yaroslav Petrov <iring2209@gmail.com>
2026-05-01 02:19:17 +0200

    Twentyseventh commit

---
+++ ДЗ 9.1. Сходи/index.js
@@ -0,0 +1,18 @@
+let ladder = {
+  step: 0,
+
+  up: function () {
+    this.step++;
+    return this;
+  },
+
+  down: function () {
+    this.step--;
+    return this;
+  },
+
+  showStep: function () {
+    console.log(this.step);
+    return this;
+  }
+};
